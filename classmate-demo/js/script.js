6eb410353ef060178731c8b6eee7aa8214a65155
nicaiasharp <nsharp@eastsideprep.org>
2024-05-28 14:57:05 -0700

    classmate demo

---
+++ classmate-demo/js/script.js
@@ -0,0 +1,7 @@
+//hamburger menu from https://alvarotrigo.com/blog/hamburger-menu-css/ demo
+
+function menuOnClick() {
+    document.getElementById("menu-bar").classList.toggle("change");
+    document.getElementById("nav").classList.toggle("change");
+    document.getElementById("menu-bg").classList.toggle("change-bg");
+  }
